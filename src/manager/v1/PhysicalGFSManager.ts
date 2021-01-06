6962ee46c386016f6fc72e687319fa392f38bbd8
Matti <contact@schmidtchen.me>
2021-01-06 11:44:15 +0100

    fixed ts errors (and hopefully it works)

--- src/manager/v1/PhysicalGFSManager.ts
+++ src/manager/v1/PhysicalGFSManager.ts
@@ -6,7 +6,7 @@ import { IResource } from '../../resource/v1/IResource'
 export class PhysicalGFSManager extends PhysicalFSManager
 {
     uid : string = 'PhysicalGFSManager_1.0.0';
-    
+
     serialize(resource : any, obj : SerializedObject) : object
     {
         if(resource.constructor !== PhysicalGateway)
@@ -23,7 +23,7 @@ export class PhysicalGFSManager extends PhysicalFSManager
     unserialize(data : any, obj : SerializedObject) : IResource
     {
         const rs = new PhysicalGateway(data.realPath, data.customName, null, this);
-        
+
         rs.dateCreation = data.dateCreation;
         rs.dateLastModified = data.dateLastModified;
         rs.properties = data.properties;
